eb2fe0b6e2f4950a24a9f8574d3c429fd44629ad
Mattias <mattias@mattias.to>
2018-04-04 23:32:30 +0200

    Added converter log message

--- src/main/java/to/mattias/services/Converter.java
+++ src/main/java/to/mattias/services/Converter.java
@@ -1,5 +1,7 @@
 package to.mattias.services;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import java.io.IOException;
 
@@ -8,8 +10,11 @@ import java.io.IOException;
  */
 @Service
 public class Converter {
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
     public String convert(String file, int start, int end) {
     	  String outFile = file.substring(0, file.lastIndexOf("."));
+    	  logger.info("Converting {} and saving it to {}", file, outFile);
     	  // Command for running ffmpeg
         String command = "ffmpeg -i " + file + " -ss 00:00:" + start + " -t 00:00:" + end + " " + outFile + ".gif";
         try {
