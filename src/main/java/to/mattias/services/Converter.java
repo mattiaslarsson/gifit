9bc57f31dbf98a1ed1f26472fa05d1ba3e159683
Mattias Larsson <mattias.larsson.9@wirelesscar.com>
2019-03-02 08:54:21 +0100

    Some minor codestyle compliance refactoring Removed field injection in FileStorage

--- src/main/java/to/mattias/services/Converter.java
+++ src/main/java/to/mattias/services/Converter.java
@@ -1,35 +1,37 @@
 package to.mattias.services;
 
+import java.io.IOException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-import java.io.IOException;
 
 /**
  * <h1>Created by Mattias on 2017-03-10.</h1>
  */
 @Service
 public class Converter {
-    private Logger logger = LoggerFactory.getLogger(getClass());
 
-    public String convert(String file, int start, int end) {
-    	  String outFile = file.substring(0, file.lastIndexOf("."));
-    	  logger.info("Converting {} and saving it to {}", file, outFile);
-    	  // Command for running ffmpeg
-        String command = "ffmpeg -i " + file + " -ss 00:00:" + start + " -t 00:00:" + end + " " + outFile + ".gif";
-        try {
-            // Execute the ffmpeg command
-            Process p = Runtime.getRuntime().exec(command);
-            p.waitFor();
-            // Remove the old file
-            Process p2 = Runtime.getRuntime().exec("rm " + file);
-            p2.waitFor();
+  private Logger logger = LoggerFactory.getLogger(getClass());
 
-            return outFile + ".gif";
-        } catch (IOException  | InterruptedException e) {
-            e.printStackTrace();
-        }
+  public String convert(String file, int start, int end) {
+    String outFile = file.substring(0, file.lastIndexOf("."));
+    logger.info("Converting {} and saving it to {}", file, outFile);
+    // Command for running ffmpeg
+    String command =
+        "ffmpeg -i " + file + " -ss 00:00:" + start + " -t 00:00:" + end + " " + outFile + ".gif";
+    try {
+      // Execute the ffmpeg command
+      Process p = Runtime.getRuntime().exec(command);
+      p.waitFor();
+      // Remove the old file
+      Process p2 = Runtime.getRuntime().exec("rm " + file);
+      p2.waitFor();
 
-        return null;
+      return outFile + ".gif";
+    } catch (IOException | InterruptedException e) {
+      e.printStackTrace();
     }
+
+    return null;
+  }
 }
--- src/main/java/to/mattias/services/FileStorage.java
+++ src/main/java/to/mattias/services/FileStorage.java
@@ -1,34 +1,40 @@
 package to.mattias.services;
 
+import static java.lang.System.currentTimeMillis;
+
+import java.io.File;
+import java.io.IOException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.io.File;
-import java.io.IOException;
 
 /**
  * Created by mattias on 2017-03-10.
  */
 @Component
 public class FileStorage {
-    @Autowired
-    private Environment env;
 
-    public String save(MultipartFile fileToStore) throws IOException {
+  private final Environment env;
 
-        String location = env.getProperty("spring.http.multipart.location");
-        // Generate a unique filename for the uploaded file and save the uploaded file
-        String filename = String.valueOf(System.currentTimeMillis()) + ".mp4";
-        File file = new File(filename);
-        fileToStore.transferTo(file);
+  @Autowired
+  public FileStorage(Environment env) {
+    this.env = env;
+  }
 
-        return location + "/" + filename;
-    }
+  public String save(MultipartFile fileToStore) throws IOException {
 
-    public void createUploadDir() {
-      String location = env.getProperty("spring.http.multipart.location");
-      new File(location).mkdir();
-    }
+    String location = env.getProperty("spring.http.multipart.location");
+    // Generate a unique filename for the uploaded file and save the uploaded file
+    String filename = currentTimeMillis() + ".mp4";
+    File file = new File(filename);
+    fileToStore.transferTo(file);
+
+    return location + "/" + filename;
+  }
+
+  public void createUploadDir() {
+    String location = env.getProperty("spring.http.multipart.location");
+    new File(location).mkdir();
+  }
 }
